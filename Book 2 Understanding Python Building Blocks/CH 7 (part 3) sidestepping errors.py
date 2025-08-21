f552e5794f58fc0d1c671a6467a816f8aa2dea55
Aie <lokotoko99@hotmail.com>
2025-08-21 18:26:25 -0400

    August 21 2025 updates

---
+++ Book 2 Understanding Python Building Blocks/CH 7 (part 3) sidestepping errors.py
@@ -0,0 +1,40 @@
+"""
+try:
+try to do this
+except ErrorType as e:
+if ErrorType happens, execute this
+except:
+if something else bad happens, execute this
+else:
+if no exceptions, continue on normally here
+finally:
+do this code no matter what happened above
+
+"""
+
+# A variable that stars out False.
+
+file_is_open = False
+
+try:
+    # Open the file named people.csv
+    the_file = open("peoplefinal.csv")
+
+# Watch for common error and stop program if it happens.
+except FileNotFoundError:
+    print("Sorry, I don't see a file named people.csv here")
+
+# OTherwise, if nothing bad has happened by now, just keep going.
+else:
+    # File must be open by now. Set the open status to True.
+    file_is_open = True
+
+    # Show the file contents.
+    for one_line in the_file:
+        print(one_line)
+
+finally:
+    # This executes no matter what. So if the file is open, it's closed.
+    if file_is_open:
+        the_file.close()
+        print("File is closed now")
---
+++ Book 2 Understanding Python Building Blocks/CH 7 (part 4) sidestepping errors.py
@@ -0,0 +1,37 @@
+# Custom exception handler inherits from Exception class.
+class CustomException(Exception):
+    def __init__(self, message = "A custom exception occured"):
+        self.message = message
+        super().__init__(self.message)
+# A variable that starts out False.
+file_is_open = False
+
+try:
+    # Open the file named people.csv
+    the_file = open("test.csv")
+    # Count the number of lines in file.
+    line_count = len(the_file.readlines())
+    # If there are fewer than 2 lines. raise exception.
+    if line_count < 2:
+        raise CustomException('Not enough rows!')
+# Handles missing file rror.
+except FileNotFoundError:
+    print('\nThere is no peoplefinal.csv file here')
+
+# Handles all other exceptions
+except Exception as e:
+    print('\n\nAn unexpected error occured.')
+
+# Otherwise, if nothing bad has happened by now, just keep going.
+else:
+    # File must be open by now. Set the open status to True.
+    file_is_open = True
+    the_file.seek(0) # Reset the file pointer to the beginning of the file.
+    for one_line in the_file:
+        print(one_line)
+
+finally:
+    #This executes no matter what. So the file is closed if it was open.
+    if file_is_open:
+        the_file.close()
+    print("File is closed now")
